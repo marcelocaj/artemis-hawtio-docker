42f1a1e103c34d073d60bf104b95ac396521e481
marcelocaj <marcelocaj@gmail.com>
2017-10-06 12:15:53 -0500

    removed hawtio for 2.3 version since it's already included

--- init.sh
+++ init.sh
@@ -20,21 +20,21 @@ cd ${ARTEMIS_INSTANCE_DIR}/etc && \
     -v "http://0.0.0.0:8161" bootstrap.xml
 
 # add hawtio and artemis plugin to bootstrap.xml
-cd ${ARTEMIS_INSTANCE_DIR}/etc && \
-    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
-       -s /amq:broker/amq:web -t elem -n app -v "" \
-       -i /amq:broker/amq:web/app -t attr -n url -v hawtio \
-       -i /amq:broker/amq:web/app -t attr -n war -v "hawtio.war" bootstrap.xml && \
-    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
-       -s /amq:broker/amq:web -t elem -n app -v "" \
-       -i /amq:broker/amq:web/app -t attr -n url -v artemis-plugin \
-       -i /amq:broker/amq:web/app -t attr -n war -v "artemis-plugin.war" bootstrap.xml  && \
-    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
-       -s /amq:broker/amq:web -t elem -n app -v "" \
-       -i /amq:broker/amq:web/app -t attr -n url -v dispatch-hawtio-console \
-       -i /amq:broker/amq:web/app -t attr -n war -v "dispatch-hawtio-console.war" bootstrap.xml
+#cd ${ARTEMIS_INSTANCE_DIR}/etc && \
+#    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
+#       -s /amq:broker/amq:web -t elem -n app -v "" \
+#       -i /amq:broker/amq:web/app -t attr -n url -v hawtio \
+#       -i /amq:broker/amq:web/app -t attr -n war -v "hawtio.war" bootstrap.xml && \
+#    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
+#       -s /amq:broker/amq:web -t elem -n app -v "" \
+#       -i /amq:broker/amq:web/app -t attr -n url -v artemis-plugin \
+#       -i /amq:broker/amq:web/app -t attr -n war -v "artemis-plugin.war" bootstrap.xml  && \
+#    xmlstarlet ed -L -N amq="http://activemq.org/schema" \
+#       -s /amq:broker/amq:web -t elem -n app -v "" \
+#       -i /amq:broker/amq:web/app -t attr -n url -v dispatch-hawtio-console \
+#       -i /amq:broker/amq:web/app -t attr -n war -v "dispatch-hawtio-console.war" bootstrap.xml
 
-cd ${ARTEMIS_INSTANCE_DIR}/etc && sed -i 's/\(^JAVA_ARGS=.*\)"/\1 \${HAWTIO_OPTS} \${JAVA_EXTRA_OPTS}"/g' artemis.profile
+#cd ${ARTEMIS_INSTANCE_DIR}/etc && sed -i 's/\(^JAVA_ARGS=.*\)"/\1 \${HAWTIO_OPTS} \${JAVA_EXTRA_OPTS}"/g' artemis.profile
 
 chown -R artemis.artemis ${ARTEMIS_INSTANCE_DIR}
 
